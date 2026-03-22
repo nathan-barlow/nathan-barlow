c0b897f3026f8d4f4b2e33fddecd508d4230f8e8
Nate Barlow <52257830+nathan-barlow@users.noreply.github.com>
2026-03-21 21:27:11 -0500

    github deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    // site: "https://nathan-barlow.github.io",
-    // base: "/natebarlow",
+    site: "https://nathan-barlow.github.io",
+    base: "/natebarlow",
 });
